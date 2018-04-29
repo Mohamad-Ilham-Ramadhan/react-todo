5e715fe2bedc8696e2f525e5ee12a7fc70c1a1a9
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2018-04-29 23:23:27 +0700

    Composing configuration (webpack) separating CSS/extract CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,63 @@
 const webpack = require('webpack'); 
+const merge = require('webpack-merge');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	entry: {
-		app: path.join(__dirname, 'app'),
-	},
-	output: {
-		path: path.join(__dirname, 'build'),
-		filename: '[name].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(js|jsx)$/,
-				use: ['babel-loader'],
-				exclude: /node_modules/
-			}
+const parts = require('./webpack.parts');
+
+const common = merge([
+	{
+		entry: {
+			app: path.join(__dirname, 'app'),
+		},
+		output: {
+			path: path.join(__dirname, 'build'),
+			filename: '[name].js',
+		},
+		resolve: {
+			extensions: ['*', '.js', '.jsx']
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: 'todo',
+				template: require('html-webpack-template'),
+				appMountId: 'app'
+			}),
+			new webpack.HotModuleReplacementPlugin()
 		]
 	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx']
+]);
+
+const development = merge([
+	{
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.(js|jsx)$/,
+					use: ['babel-loader'],
+					exclude: /node_modules/
+				}
+			]
+		},
+		devServer: {
+			hot: true
+		},
 	},
-	devServer: {
-		hot: true
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'todo',
-			template: require('html-webpack-template'),
-			appMountId: 'app'
-		}),
-		new webpack.HotModuleReplacementPlugin()
-	]
+]);
+
+const production = merge([
+	parts.loadJavaScript(),
+	parts.extractCSS(),
+]);
+
+module.exports = mode => {
+	if ( mode === "production" ) {
+		return merge( common, production, { mode });
+	}
+
+	return merge( common, development, { mode });
 }
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,2 +1,48 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
+exports.loadJavaScript = () => ({
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader'
+			}
+		]
+	}
+})
+
+exports.autoprefix = () => ({
+	loader: 'postcss-loader',
+	options: {	
+		plugins: [autoprefixer]
+	}
+})
+
+exports.extractCSS = () => {
+	const plugin = new ExtractTextPlugin({
+		filename: '[name].css',
+	});
+
+	return {
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: plugin.extract({
+						use: ['css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: [autoprefixer]
+							}
+						}],
+						fallback: 'style-loader'
+					}),
+				}
+			]
+		},
+		plugins: [plugin]
+	}
+}
 
